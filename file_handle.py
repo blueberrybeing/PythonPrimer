ef246bc055a332cc1c3c64d33be98aae8d276634
hx <jingyonggang@51yuyou.com>
2018-04-04 20:01:35 +0800

    dkdk

--- file_handle.py
+++ file_handle.py
@@ -3,7 +3,7 @@ import random
 import string
 
 def random_file_name():
-	return ''.join(random.sample(string.ascii_letters + string.digits, 8))
+    return ''.join(random.sample(string.ascii_letters + string.digits, 8))
 
 file_dict = {}
 def traverse(f):
@@ -19,13 +19,19 @@ def traverse(f):
 path = '/Users/hx/Desktop/res_name/res'
 traverse(path)
 
+def write_to_lua():
+    with open('file_dict.lua', 'w') as f:
+        f.write("local file_dict = {\n")
+        for key,value in file_dict.items():
+            print('key is %s, value is %s' %(key, value))
+            temp = key + '=' + value + '\n'
+            f.write(temp)
+        f.write("}\n")
+        f.write("return file_dict\n")
 
-
-
-for key,value in file_dict.items():
-    print('key is %s, value is %s' %(key, value))
+write_to_lua()
 
 
 # for x in range(100):
-# 	salt = random_file_name()
-# 	print(salt)
+#   salt = random_file_name()
+#   print(salt)
